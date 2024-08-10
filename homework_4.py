f6fb70e413be89ea7de18685d4a397dbd5b6966b
Bogolepov69 <168251335+Bogolepov69@users.noreply.github.com>
2024-08-10 06:46:52 +0300

    Add files via upload

---
+++ homework_4.py
@@ -0,0 +1,23 @@
+import inspect
+
+
+def introspection_info(obj):
+    obj_type = type(obj).__name__
+
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    methods = [method for method in dir(obj) if callable(getattr(obj, method)) ]
+
+    obj_module = obj.__mod__
+
+    info = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': methods,
+        'module': obj_module
+    }
+
+    return info
+
+
+number_info = introspection_info(42)
+print(number_info)
